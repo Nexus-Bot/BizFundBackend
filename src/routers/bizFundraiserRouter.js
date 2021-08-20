0e3f2bca403a95290e4c24f019a62061d467e7c8
INNOVATIVEGAMER <55017197+INNOVATIVEGAMER@users.noreply.github.com>
2021-08-21 01:13:54 +0530

    Created Full authentication backend and milestone model.
    1. Created ProjectMaker model
    2. Created BizFundraiser model
    3. Created Milestone model
    4. Created authentication middlewares for ProjectMaker & BizFundraiser
    5. Created routers with all CRUD operations for ProjectMaker & BizFundraiser

---
+++ src/routers/bizFundraiserRouter.js
@@ -0,0 +1,155 @@
+const express = require("express")
+const BizFundraiser = require("../models/bizFundraiserModel")
+const auth = require("../middlewares/authBizFundraiser")
+// const { sendWelcomeMail, sendAccountDeleteMail } = require('../mail/account');
+
+const router = express.Router()
+
+//Create bizFundraiser in DB
+router.post("/bizfundraisers", async (req, res) => {
+    try {
+        const bizFundraiser = new BizFundraiser(req.body)
+        const bizFundraiserDoc = await bizFundraiser.save()
+        const token = await bizFundraiser.generateAuthenticationToken()
+        // sendWelcomeMail(bizFundraiser.name, bizFundraiser.email);
+        res.status(201).send({ bizFundraiserDoc, token })
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+//LogIn bizFundraiser
+router.post("/bizfundraisers/login", async (req, res) => {
+    try {
+        const bizFundraiser = await BizFundraiser.findByCredentials(
+            req.body.email,
+            req.body.password
+        )
+
+        const token = await bizFundraiser.generateAuthenticationToken()
+
+        res.send({ bizFundraiser, token })
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+})
+
+//LogOut bizFundraiser from one session
+router.post("/bizfundraisers/logout", auth, async (req, res) => {
+    try {
+        req.bizFundraiser.tokens = req.bizFundraiser.tokens.filter(
+            (token) => token.token !== req.token
+        )
+
+        await req.bizFundraiser.save()
+
+        res.status(200).send("Successfully logged out :)")
+    } catch (error) {
+        res.status(500).send({ error: "Error logging out!!!" })
+    }
+})
+
+//LogOut bizFundraiser from all sessions
+router.post("/bizfundraiserss/logoutall", auth, async (req, res) => {
+    try {
+        req.bizFundraiser.tokens = []
+        await req.bizFundraiser.save()
+
+        res.status(200).send("Successfully logged out from all sessions :)")
+    } catch (error) {
+        res.status(500).send({ error: "Error logging out!!!" })
+    }
+})
+
+//Get all bizFundraisers in DB
+router.get("/bizfundraisers", async (req, res) => {
+    try {
+        const bizfundraisers = await BizFundraiser.find({})
+        if (!bizfundraisers) {
+            res.status(404).send({ error: "BizFundraisers not found!!!" })
+            return
+        }
+
+        res.send(bizfundraisers)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+//Get current authenticated bizFundraiser
+router.get("/bizfundraisers/me", auth, (req, res) => {
+    res.send(req.bizFundraiser)
+})
+
+//Get bizFundraiser by ID from DB
+router.get("/bizfundraisers/:id", auth, async (req, res) => {
+    try {
+        const _id = req.params.id
+        const bizFundraiser = await BizFundraiser.findById(_id)
+
+        if (!bizFundraiser) {
+            res.status(404).send({ error: "BizFundraiser not found!!!" })
+            return
+        }
+        res.send(bizFundraiser)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+//Update current authenticated bizFundraiser in DB
+router.patch("/bizfundraisers/me", auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = [
+        "firstName",
+        "lastName",
+        "displayName",
+        "password",
+        "photoURL",
+        "metamaskAddress",
+        "isVerified",
+    ]
+
+    const isValidUpdates = updates.every((update) =>
+        allowedUpdates.includes(update)
+    )
+
+    if (!isValidUpdates) {
+        res.status(400).send({ error: "invalid updates" })
+        return
+    }
+
+    try {
+        const bizFundraiser = req.bizFundraiser
+
+        updates.forEach((update) => {
+            bizFundraiser[update] = req.body[update]
+        })
+
+        const updatedBizFundraiser = await bizFundraiser.save()
+
+        res.send(updatedBizFundraiser)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+//Delete current authenticated bizFundraiser from DB
+router.delete("/bizfundraisers/me", auth, async (req, res) => {
+    try {
+        const _id = req.bizFundraiser._id
+        const bizFundraiser = await BizFundraiser.findById(_id)
+        if (!bizFundraiser) {
+            res.status(404).send({ error: "BizFundraiser not found!!!" })
+            return
+        }
+
+        const deletedBizFundraiser = await bizFundraiser.remove()
+        // sendAccountDeleteMail(deletedBizFundraiser.name, deletedBizFundraiser.email);
+        res.send(deletedBizFundraiser)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+module.exports = router
